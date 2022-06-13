bb4f0a0f92b4b6cd5c32187003e712b8bc45bea6
yse-yoo <71994236+yse-yoo@users.noreply.github.com>
2022-06-13 10:11:45 +0900

    add App

--- src/character/App.java
+++ src/character/App.java
@@ -3,7 +3,13 @@ package character;
 public class App {
 
 	public static void main(String[] args) {
-
+		Wizard wizard = new Wizard();
+		wizard.greet("こんにちは");
+		
+		Warrior worrior = new Warrior();
+		worrior.greet("どーも");
+		
+		
 	}
 
 }
