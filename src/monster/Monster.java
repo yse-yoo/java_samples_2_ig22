73371d337fc3719b5d45ae02bafa98dd5e66a9ee
yse-yoo <71994236+yse-yoo@users.noreply.github.com>
2022-06-13 14:14:43 +0900

    add Monster

--- src/monster/Monster.java
+++ src/monster/Monster.java
@@ -8,5 +8,20 @@ public class Monster {
 	public int mp;
 	public int attackPower;
 	public int defencePower;
+	
+	//コンストラクタ
+	public Monster() {
+		super();
+		
+		this.name = "ゴースト";
+		this.hp = 5;
+		this.mp = 0;
+		this.attackPower = 2;
+		this.defencePower = 1;
+	}
 
+	public boolean isAlive() {
+		return (this.hp > 0);
+	}
+	
 }
