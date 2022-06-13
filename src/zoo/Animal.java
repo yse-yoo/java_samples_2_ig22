8bb7225683675948b6283184e49e33f698ad8d08
yse-yoo <71994236+yse-yoo@users.noreply.github.com>
2022-06-13 11:34:25 +0900

    Add Animal class

---
+++ src/zoo/Animal.java
@@ -0,0 +1,23 @@
+package zoo;
+
+public class Animal {
+	
+	//フィールド
+	public String type;
+	public String name;
+	public String crying;
+	
+	
+	//インスタンスメソッド
+	public void walk() {
+		
+	}
+
+	public void cry() {
+		
+	}
+	
+	public void escape() {
+		
+	}
+}
