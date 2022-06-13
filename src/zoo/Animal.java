aed8c0ce27319375e71db09efff1ddd12d66cca2
yse-yoo <71994236+yse-yoo@users.noreply.github.com>
2022-06-13 11:39:37 +0900

    add method

--- src/zoo/Animal.java
+++ src/zoo/Animal.java
@@ -10,14 +10,16 @@ public class Animal {
 	
 	//インスタンスメソッド
 	public void walk() {
-		
+		String message = this.name + "が歩いた";
+		System.out.println(message);
 	}
 
 	public void cry() {
-		
+		System.out.println(this.crying);
 	}
 	
 	public void escape() {
-		
+		String message = this.name + "が逃げた";
+		System.out.println(message);
 	}
 }
