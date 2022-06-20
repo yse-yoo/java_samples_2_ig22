bf955744b01df59284ac337595072a9fe3ae0171
yse-yoo <71994236+yse-yoo@users.noreply.github.com>
2022-06-20 10:45:15 +0900

    add Cat Override method

--- src/zoo/Cat.java
+++ src/zoo/Cat.java
@@ -9,6 +9,11 @@ public class Cat extends Animal {
 		this.crying = "にゃー";
 	}
 
+	@Override
+	public void walk() {
+		String message = this.name + "警戒して歩いています。";
+		System.out.println(message);
+	}
 	
 	//単独行動
 	public void soloAction() {
