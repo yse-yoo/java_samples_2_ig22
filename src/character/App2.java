eaef9be80b226c945b40a8108aff1a3093492ab3
yse-yoo <71994236+yse-yoo@users.noreply.github.com>
2022-06-20 13:15:09 +0900

    add ArrayList

--- src/character/App2.java
+++ src/character/App2.java
@@ -1,9 +1,21 @@
 package character;
 
+import java.util.ArrayList;
+
 public class App2 {
 
 	public static void main(String[] args) {
+		//配列 Array(imutable)
+		//Character[] characters;
 		
+		//リスト(Collection)の定義 (mutable)
+		ArrayList<Character> characterList = new ArrayList<>();
+
+		//データ追加 add()
+		characterList.add(new Wizard("アリス"));
+		characterList.add(new Wizard("テリー"));
+		characterList.add(new Warrior("ボブ"));
+
 	}
 
 }
