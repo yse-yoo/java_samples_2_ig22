1de6356bb24475218d61c6e6aa27903a049e8ebc
yse-yoo <71994236+yse-yoo@users.noreply.github.com>
2022-06-20 14:13:08 +0900

    CRUD memo

--- src/character/App2.java
+++ src/character/App2.java
@@ -5,6 +5,11 @@ import java.util.ArrayList;
 public class App2 {
 
 	public static void main(String[] args) {
+		//CRUD
+		//C: Create
+		//R: Read
+		//U: Update
+		//D: Delete(Destroy)
 
 		//配列 Array(imutable)
 		//Character[] characters;
@@ -12,7 +17,7 @@ public class App2 {
 		//リスト(Collection)の定義 (mutable)
 		ArrayList<Character> characterList = new ArrayList<>();
 
-		//データ追加 add()
+		//データ追加 add() => create
 		characterList.add(new Wizard("アリス"));
 		characterList.add(new Wizard("テリー"));
 		characterList.add(new Warrior("ボブ"));
@@ -25,11 +30,11 @@ public class App2 {
 		int dataCount = characterList.size();
 		System.out.println(dataCount);
 		
-		//データの取得: get(index)
+		//データの取得: get(index) => read
 		Character selectCharacter = characterList.get(1);
 		System.out.println(selectCharacter.name);
 		
-		//最後のデータ削除
+		//最後のデータ削除: remove() => delete
 		//最後のインデクスを取得
 		int lastIndex = dataCount - 1;
 		characterList.remove(lastIndex);
@@ -39,7 +44,17 @@ public class App2 {
 			System.out.println(character.name);
 		}
 		
+
+		//データ更新: set() => update
+		System.out.println("--- データ更新 ---");
+		characterList.set(0, new Warrior("スミス"));
+		for (Character character : characterList) {
+			System.out.println(character.name);
+		}
+
+		
 		//すべて削除
+		System.out.println("--- データ全削除 ---");
 		characterList.clear();
 		dataCount = characterList.size();
 		System.out.println(dataCount);	
