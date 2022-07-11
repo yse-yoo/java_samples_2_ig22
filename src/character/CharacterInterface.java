511637fff615215c954c07efffd6e910cd02eda5
yse-yoo <71994236+yse-yoo@users.noreply.github.com>
2022-07-11 13:55:20 +0900

    add Interface method

--- src/character/CharacterInterface.java
+++ src/character/CharacterInterface.java
@@ -1,5 +1,12 @@
 package character;
 
+import monster.Monster;
+
 public interface CharacterInterface {
 
+	public void attack(Monster monster);
+
+	public boolean isAlive();
+
+	public void magic(Monster monster, String key);
 }
