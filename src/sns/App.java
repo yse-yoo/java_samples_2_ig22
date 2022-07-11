112f37eae87b6c173c3a97d287a91169066f7010
yse-yoo <71994236+yse-yoo@users.noreply.github.com>
2022-07-11 09:35:17 +0900

    app setter/getter

--- src/sns/App.java
+++ src/sns/App.java
@@ -6,6 +6,14 @@ public class App {
 
 	public static void main(String[] args) {
 		UserBean user = new UserBean();
+//		user.email = "test@example.com";
+
+		//setter
+		user.setEmail("test@example.com");
+
+		//getter
+		String email = user.getEmail();
+		System.out.println(email);
 	}
 
 }
