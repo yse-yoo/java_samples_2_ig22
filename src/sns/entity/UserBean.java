0e0f3e520fd7ddf4262028dc9cf33494a95b11fc
yse-yoo <71994236+yse-yoo@users.noreply.github.com>
2022-07-11 09:26:19 +0900

    add getter/setter

--- src/sns/entity/UserBean.java
+++ src/sns/entity/UserBean.java
@@ -6,4 +6,25 @@ public class UserBean {
 	private String name;
 	private String password;
 	
+	//getter / setter (自動で作成）
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	
 }
