66ed6822f78b482d63afad808c0381d3116bf9b4
yse-yoo <71994236+yse-yoo@users.noreply.github.com>
2022-09-26 13:07:03 +0900

    add Iterator

--- src/cafe/AppIterator.java
+++ src/cafe/AppIterator.java
@@ -1,11 +1,14 @@
 package cafe;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 
 public class AppIterator {
 
 	public static void main(String[] args) {
+		System.out.println("--- ArrayList Iterator ---");
 		ArrayList<Drink> drinkList = new ArrayList<>();
 		drinkList.add(new Drink("コーヒー", 350));
 		drinkList.add(new Drink("紅茶", 400));
@@ -17,6 +20,20 @@ public class AppIterator {
 			Drink drink = iterator.next();
 			System.out.println(drink.name);
 		}
+
+		//Mapデータ
+		System.out.println("--- Map Iterator ---");
+		Map<Integer, Drink> drinkMap = new HashMap<>();
+		drinkMap.put(1, new Drink("コーヒー", 350));
+		drinkMap.put(2, new Drink("紅茶", 400));
+		drinkMap.put(3, new Drink("ほうじ茶", 300));
+		
+		Iterator<Integer> mapIterator = drinkMap.keySet().iterator();
+		while (mapIterator.hasNext()) {
+			Integer key = mapIterator.next();
+			System.out.println(key);
+		}
+
 
 	}
 
