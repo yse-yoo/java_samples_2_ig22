fe544007fc1f56333c9e4a3060e9ab4bc9173f15
yse-yoo <71994236+yse-yoo@users.noreply.github.com>
2022-09-26 09:31:31 +0900

    update

--- src/cafe/AppMap.java
+++ src/cafe/AppMap.java
@@ -1,8 +1,14 @@
 package cafe;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AppMap {
 
 	public static void main(String[] args) {
+//		ArrayList<Drink> drinkList = new ArrayList<>();
+
+		Map<Integer, Drink> drinkMap = new HashMap<>();
 
 	}
 
--- src/cafe/Drink.java
+++ src/cafe/Drink.java
@@ -11,5 +11,4 @@ public class Drink {
 		this.price = price;
 	}
 
-
 }
