5f6d4b8e91305988b04dba6f4bbfddef9e527068
yse-yoo <71994236+yse-yoo@users.noreply.github.com>
2022-10-03 11:48:14 +0900

    add app

---
+++ src/cafe/App3.java
@@ -0,0 +1,15 @@
+package cafe;
+
+public class App3 {
+
+	public static void main(String[] args) {
+		Drink drink1 = new Drink("コーヒー", 350, Size.SMALL);
+		Drink drink2 = new Drink("コーヒー", 400, Size.MEDIUM);
+		Drink drink3 = new Drink("コーヒー", 450, Size.LARGE);
+		
+		System.out.println(drink1.size);
+		System.out.println(drink2.size);
+		System.out.println(drink3.size);
+	}
+
+}
