fda2748d757beda0caa9c7e6299c2bb013088752
yse-yoo <71994236+yse-yoo@users.noreply.github.com>
2022-10-03 11:37:16 +0900

    add Drink size

--- src/cafe/Drink.java
+++ src/cafe/Drink.java
@@ -4,11 +4,19 @@ public class Drink {
 
 	public String name;
 	public Integer price;
+	public Size size;
 
 	public Drink(String name, Integer price) {
 		super();
 		this.name = name;
 		this.price = price;
+	}
+
+	public Drink(String name, Integer price, Size size) {
+		super();
+		this.name = name;
+		this.price = price;
+		this.size = size;
 	}
 
 }
