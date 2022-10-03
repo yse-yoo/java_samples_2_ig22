777e188098848b74e8e146aa3326b112e85f2c74
yse-yoo <71994236+yse-yoo@users.noreply.github.com>
2022-10-03 12:05:43 +0900

    add getValue()

--- src/cafe/Size.java
+++ src/cafe/Size.java
@@ -1,5 +1,16 @@
 package cafe;
 
 public enum Size {
-	LARGE, MEDIUM, SMALL
+	LARGE("大"), MEDIUM("中"), SMALL("小");
+	
+	private final String value;
+	
+	private Size(String value) {
+		this.value = value;
+	}
+	
+	public String getValue() {
+		return this.value;
+	}
+
 }
