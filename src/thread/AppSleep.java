8c6d954fea4d2027864bfe0d5d6c6d2bfaead32d
yse-yoo <71994236+yse-yoo@users.noreply.github.com>
2022-10-17 09:31:12 +0900

    add drinkList

--- src/thread/AppSleep.java
+++ src/thread/AppSleep.java
@@ -1,9 +1,18 @@
 package thread;
 
+import java.util.ArrayList;
+
 public class AppSleep {
 
     public static void main(String[] args) {
-
+        ArrayList<Item> drinkList = new ArrayList<>();
+        drinkList.add(new Item("コーヒー"));
+        drinkList.add(new Item("紅茶"));
+        drinkList.add(new Item("ほうじ茶"));
+        
+        for (Item item : drinkList) {
+            item.order();
+        }
     }
 
 }
