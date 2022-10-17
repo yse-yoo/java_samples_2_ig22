b36ab10df8c9b441080a86830f5c4b0b856b0fd9
yse-yoo <71994236+yse-yoo@users.noreply.github.com>
2022-10-17 10:59:47 +0900

    add run()

--- src/thread/CounterThread.java
+++ src/thread/CounterThread.java
@@ -7,8 +7,17 @@ public class CounterThread implements Runnable {
 
     @Override
     public void run() {
-        
+        while (count > 0) {
+            try {
+                Thread.sleep(ms);
+                System.out.println(Thread.currentThread().getName() + ":" + count);
+                count--;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        count = 5;
+        System.out.println("End thread");
     }
-
     
 }
