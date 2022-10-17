abd6389bc8e13464dec032f3e3d2c4ef441ae596
yse-yoo <71994236+yse-yoo@users.noreply.github.com>
2022-10-17 11:08:23 +0900

    add main thread

--- src/thread/AppCounter.java
+++ src/thread/AppCounter.java
@@ -3,7 +3,11 @@ package thread;
 public class AppCounter {
 
     public static void main(String[] args) {
-
+        CounterThread counter = new CounterThread();
+        
+        Thread thread1 = new Thread(counter);
+        thread1.start();
+        
     }
 
 }
