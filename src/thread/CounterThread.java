374f46650c4ec509dc9da94956ef11dee8e491d7
yse-yoo <71994236+yse-yoo@users.noreply.github.com>
2022-10-17 10:52:21 +0900

    impolments Runnabel

--- src/thread/CounterThread.java
+++ src/thread/CounterThread.java
@@ -1,8 +1,14 @@
 package thread;
 
-public class CounterThread {
+public class CounterThread implements Runnable {
 
     public static Integer count = 5;
     public long ms = 1000;
+
+    @Override
+    public void run() {
+        
+    }
+
     
 }
