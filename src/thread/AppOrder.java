9973b26ac277c5cbfaeaa5e3605445df87015636
yse-yoo <71994236+yse-yoo@users.noreply.github.com>
2022-10-17 10:20:36 +0900

    add start()

--- src/thread/AppOrder.java
+++ src/thread/AppOrder.java
@@ -1,9 +1,19 @@
 package thread;
 
+import java.util.ArrayList;
+
 public class AppOrder {
 
     public static void main(String[] args) {
-
+        ArrayList<Item> drinkList = new ArrayList<>();
+        drinkList.add(new Item("コーヒー"));
+        drinkList.add(new Item("紅茶"));
+        drinkList.add(new Item("ほうじ茶"));
+        
+        for (Item item : drinkList) {
+            ShopThread thread = new ShopThread(item);
+            thread.start();
+        }
     }
 
 }
