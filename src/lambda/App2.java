69df22338d61439fc30c226864c76a6dd7208bab
yse-yoo <71994236+yse-yoo@users.noreply.github.com>
2022-10-24 09:27:54 +0900

    add App2

--- src/lambda/App2.java
+++ src/lambda/App2.java
@@ -4,6 +4,19 @@ public class App2 {
 
     public static void main(String[] args) {
 
+        ICalculate calculate = new ICalculate() {
+            @Override
+            public Integer totalPrice(Integer price) {
+                Float totalPrice = price * 1.1f;
+                return totalPrice.intValue();
+            }
+        };
+            
+        Integer price = calculate.totalPrice(1000);
+        System.out.println(price);
+
+        price = calculate.totalPrice(2563);
+        System.out.println(price);
     }
 
 }
