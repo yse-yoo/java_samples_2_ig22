fcb4201d2c324074b91a38c43a51cf43b9bf4f35
yse-yoo <71994236+yse-yoo@users.noreply.github.com>
2022-10-24 10:08:56 +0900

    add Drink

---
+++ src/lambda/AppDrink.java
@@ -0,0 +1,9 @@
+package lambda;
+
+public class AppDrink {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/lambda/Drink.java
@@ -0,0 +1,14 @@
+package lambda;
+
+public class Drink {
+
+    public String name;
+    public Integer price;
+
+    public Drink(String name, Integer price) {
+        super();
+        this.name = name;
+        this.price = price;
+    }
+    
+}
