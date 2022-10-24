a590dd384182d5278be9f99df7ba07e1ad6138f8
yse-yoo <71994236+yse-yoo@users.noreply.github.com>
2022-10-24 10:12:40 +0900

    add ArrayList

--- src/lambda/AppDrink.java
+++ src/lambda/AppDrink.java
@@ -1,8 +1,17 @@
 package lambda;
 
+import java.util.ArrayList;
+
+import cafe.Drink;
+
 public class AppDrink {
 
     public static void main(String[] args) {
+        // cafeパッケージの AppIterator.javaからコピー
+        ArrayList<Drink> drinkList = new ArrayList<>();
+        drinkList.add(new Drink("コーヒー", 350));
+        drinkList.add(new Drink("紅茶", 400));
+        drinkList.add(new Drink("ほうじ茶", 300));
 
     }
 
