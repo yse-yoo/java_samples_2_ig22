255cf1eddc158a318b0bc2c866c576b03eeb2b52
yse-yoo <71994236+yse-yoo@users.noreply.github.com>
2022-10-24 09:53:08 +0900

    add lambda function

--- src/lambda/AppRun.java
+++ src/lambda/AppRun.java
@@ -5,6 +5,15 @@ public class AppRun {
     public static void main(String[] args) {
         Runnable runner = ()-> System.out.println("Hello!!!");
         runner.run();
+        
+        hello(() -> showMessage("こんにちは"));
     }
 
+    public static void showMessage(String message) {
+       System.out.println(message); 
+    }
+    
+    public static void hello(Runnable r) {
+        r.run();
+    }
 }
